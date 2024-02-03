1bef69bcc78971531585a4f480e9b79fe519f615
Luckyx100 <technicallucky861@gmail.com>
2024-02-03 12:43:51 +0530

    commit

--- college_py/AverageOf_3Numbers.py
+++ college_py/AverageOf_3Numbers.py
@@ -1,11 +1,21 @@
-# eval is to convert the number (string) to number (int)
-# type tells 
-'''
-  This is a multiple line comment
-'''
-num1 = eval(input("Enter number: "))
-num2 = eval(input("Enter number: "))
-# print("Type is : ",type(num2)) -> It will 
-avg = (num1+num2)/2
-print("Type is : ",type(num2))
-print("avg is ", avg)
+# # eval is to convert the number (string) to number (int)
+# # type tells 
+# '''
+#   This is a multiple line comment
+# '''
+# num1 = eval(input("Enter number: "))
+# num2 = eval(input("Enter number: "))
+# # print("Type is : ",type(num2)) -> It will 
+# avg = (num1+num2)/2
+# print("Type is : ",type(num2))
+# print("avg is ", avg)
+
+import time
+start = time.time()
+
+list = [1,2,3,4,5]
+del list
+
+end = time.time()
+
+print("Time taken: ", format(end-start))
