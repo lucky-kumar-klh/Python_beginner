4c337efde10ae1535ac4909ddb59427b4762b72e
Luckyx100 <technicallucky861@gmail.com>
2024-02-07 08:23:33 +0530

    commit

--- Basics.py
+++ Basics.py
@@ -1,10 +1,22 @@
-def isPrime( a ):
-  for i in range(2,a-1):
-    if a % i==0:
-      return True
-  return False
+# def isPrime( a ):
+#   for i in range(2,a-1):
+#     if a % i==0:
+#       return True
+#   return False
 
-x = int(input('Enter any number : '))
-if isPrime(x):
-  print(str(x)+" is Composite Number")
-else : print(str(x)+" is a Prime Number")
+# x = int(input('Enter any number : '))
+# if isPrime(x):
+#   print(str(x)+" is Composite Number")
+# else : print(str(x)+" is a Prime Number")
+dictionary = dict([
+    ("name", "lucky kumar"),
+    ("university", "KLU")
+])
+
+# But this is a Normal list
+# this_is_a_list = ([
+#     ("name", "lucky kumar"),
+#     ("university", "KLU")
+# ])
+
+print(dictionary)
