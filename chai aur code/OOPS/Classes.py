578715a4ca8f5c30051a50fa6b183215e2852eed
Luckyx100 <technicallucky861@gmail.com>
2024-02-18 23:45:09 +0530

    commit

---
+++ chai aur code/OOPS/Classes.py
@@ -0,0 +1,26 @@
+class Car:
+    # __init__ is a constructor
+    def __init__(self, brand, model):
+        # Using '__' before attribute makes it private 
+        self.__brand = brand  # self.brand => class member
+        self.model = model  # self.model => class member
+
+    # getter method
+    def get_brand(self):
+        return self.__brand
+    
+    # Polymorphism
+    def fuel_type(self):
+        return "Petrol or Diseil"
+
+    # Adding feature to this class
+    def fullname(self):
+        return f"{self.__brand} {self.model}"
+
+my_car = Car("Renault", "Duster")
+
+print(my_car.fullname())
+print(my_car.fuel_type())
+# print(my_car.__brand)  # Private attrites can't be accessed directly
+print(my_car.model)
+print(my_car.get_brand())
---
+++ chai aur code/OOPS/Inheritance.py
@@ -0,0 +1,18 @@
+from Classes import Car
+
+class Electric_car(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+
+    # Polymorphism : Same method is used in different classes
+    def fuel_type(self):
+        return "Electric Fuel"
+
+
+my_electric_car = Electric_car("Tesla", "Model S", "100 KWH")
+print(my_electric_car.fullname())
+print(my_electric_car.fuel_type())
+print(my_electric_car.battery_size)
+
+
