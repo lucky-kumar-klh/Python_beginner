ab3e2e866074d26ea86c15a8eb9e18501812e177
Luckyx100 <technicallucky861@gmail.com>
2024-03-14 18:34:28 +0530

    commit

--- sample.py
+++ sample.py
@@ -1,2 +1,6 @@
-n = 10
-print(sum(range(1, n+1)))
+from string_rotator import rotate_word
+
+rotate_by = int(input("Enter the number to rotate by: "))
+word = input("Enter the word: ")
+
+rotate_word(word, rotate_by)
---
+++ string_rotator.py
@@ -0,0 +1,14 @@
+def rotate_word(word, rotate_by):
+    ans = ""
+    for alphabet in word:
+        if alphabet.islower():
+            if ord(alphabet) + rotate_by > 122:
+                ans += chr(96 + rotate_by % 26)
+            else:
+                ans += chr(rotate_by + ord(alphabet))
+        else:  
+            if ord(alphabet) + rotate_by > 90:
+                ans += chr(64 + rotate_by % 26)
+            else:
+                ans += chr(rotate_by + ord(alphabet))
+    print("Rotated Word:",ans)
