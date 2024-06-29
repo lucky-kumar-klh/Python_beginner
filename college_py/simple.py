242d4a78fcef82c7f9fc7406345c61a6853d35d7
Lucky Kumar <162823689+lucky-kumar-klh@users.noreply.github.com>
2024-06-29 19:33:56 +0530

    commit

--- college_py/simple.py
+++ college_py/simple.py
@@ -1,8 +1,23 @@
-f, k = 8, 8
-ans = f*k
-'''
-print(ans)
-'''
-print(ans)
-print("3*4")
-print(40 - 0.1) # operation for float, int.
+def solve(arr1, arr2, n1, n2):
+    i1, i2 = 0, 0
+    ans = []
+    while i1 < n1 and i2 < n2:
+        if arr1[i1] > arr2[i2]:
+            ans.append(arr2[i2])
+            i2 += 1
+        else:
+            ans.append(arr1[i1])
+            i1 += 1
+    while i1 < n1:
+        ans.append(arr1[i1])
+        i1 += 1
+    while i2 < n2:
+        ans.append(arr2[i2])
+        i2 += 1
+    return ans
+
+
+n1, n2 = map(int, input().split())
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+print(*solve(arr1, arr2, n1, n2))
